2ddfbee7956d8e8783baa8cec717fa069b900680
kangminsung <nonasking@gmail.com>
2026-02-23 16:26:53 +0900

    feat(expenses): edit enum

--- expenses/constants.py
+++ expenses/constants.py
@@ -2,78 +2,77 @@ from django.db import models
 
 
 class ExpenseCategoryEnum(models.TextChoices):
-    BEAUTY = "beauty", "미용"
-    HOUSEHOLD = "household", "생활용품"
-    PHONE_BILL = "phone_bill", "통신비"
-    ALLOWANCE = "allowance", "자유금"
-    FOOD = "food", "식대"
-    MEDICAL = "medical", "병원비"
-    TRANSPORT = "transport", "교통비"
-    HOUSING = "housing", "주거"
-    CLOTHING = "clothing", "의복"
-    EVENT = "event", "이벤트"
-    EXERCISE = "exercise", "운동"
-    UNKNOWN = "unknown", "미상"
-    SUBSCRIPTION = "subscription", "청약"
-    SELF_DEV = "self_dev", "자기개발"
-    CELEBRATION = "celebration", "경조사"
-    ETC = "etc", "기타"
-    UNSETTLED = "unsettled", "미정산"
+    BEAUTY = "BEAUTY", "미용"
+    HOUSEHOLD = "HOUSEHOLD", "생활용품"
+    PHONE_BILL = "PHONE_BILL", "통신비"
+    ALLOWANCE = "ALLOWANCE", "자유금"
+    FOOD = "FOOD", "식대"
+    MEDICAL = "MEDICAL", "병원비"
+    TRANSPORT = "TRANSPORT", "교통비"
+    HOUSING = "HOUSING", "주거"
+    CLOTHING = "CLOTHING", "의복"
+    EVENT = "EVENT", "이벤트"
+    EXERCISE = "EXERCISE", "운동"
+    UNKNOWN = "UNKNOWN", "미상"
+    SUBSCRIPTION = "SUBSCRIPTION", "청약"
+    SELF_DEV = "SELF_DEV", "자기개발"
+    CELEBRATION = "CELEBRATION", "경조사"
+    ETC = "ETC", "기타"
+    UNSETTLED = "UNSETTLED", "미정산"
 
 
 class ExpenseSubCategoryEnum(models.TextChoices):
     # 미용
-    BEAUTY = "beauty", "미용"
+    BEAUTY = "BEAUTY", "미용"
     # 생활용품
-    HOUSEHOLD = "household", "생활용품"
+    HOUSEHOLD = "HOUSEHOLD", "생활용품"
     # 통신비
-    PHONE_BILL = "phone_bill", "통신비"
+    PHONE_BILL = "PHONE_BILL", "통신비"
     # 자유금
-    ALLOWANCE_MS = "allowance_ms", "민성자유금"
-    ALLOWANCE_MK = "allowance_mk", "민근자유금"
+    ALLOWANCE_MS = "ALLOWANCE_MS", "민성자유금"
+    ALLOWANCE_MK = "ALLOWANCE_MK", "민근자유금"
     # 식대
-    ALCOHOL = "alcohol", "주류"
-    BEVERAGE = "beverage", "음료"
-    DELIVERY = "delivery", "배달/포장"
-    DINING_OUT = "dining_out", "외식"
-    COOKING = "cooking", "요리"
-    COMPANY_MEAL = "company_meal", "회사식대"
+    ALCOHOL = "ALCOHOL", "주류"
+    BEVERAGE = "BEVERAGE", "음료"
+    DELIVERY = "DELIVERY", "배달/포장"
+    DINING_OUT = "DINING_OUT", "외식"
+    COOKING = "COOKING", "요리"
+    COMPANY_MEAL = "COMPANY_MEAL", "회사식대"
     # 병원비
-    HOSPITAL_ETC = "hospital_etc", "기타병원비"
-    REGULAR_CHECKUP = "regular_checkup", "정기진료"
+    HOSPITAL_ETC = "HOSPITAL_ETC", "기타병원비"
+    REGULAR_CHECKUP = "REGULAR_CHECKUP", "정기진료"
     # 교통비
-    TRANSPORT = "transport", "교통비"
+    TRANSPORT = "TRANSPORT", "교통비"
     # 주거
-    ELECTRICITY = "electricity", "전기요금"
-    WATER = "water", "수도요금"
-    GAS = "gas", "가스요금"
-    MAINTENANCE = "maintenance", "관리비"
-    LEASE_INTEREST = "lease_interest", "전세이자"
-    INTERNET = "internet", "인터넷요금"
+    ELECTRICITY = "ELECTRICITY", "전기요금"
+    WATER = "WATER", "수도요금"
+    GAS = "GAS", "가스요금"
+    MAINTENANCE = "MAINTENANCE", "관리비"
+    LEASE_INTEREST = "LEASE_INTEREST", "전세이자"
+    INTERNET = "INTERNET", "인터넷요금"
     # 의복
-    CLOTHING_BUY = "clothing_buy", "의복구입"
-    CLOTHING_CARE = "clothing_care", "의복관리"
+    CLOTHING_BUY = "CLOTHING_BUY", "의복구입"
+    CLOTHING_CARE = "CLOTHING_CARE", "의복관리"
     # 이벤트
-    VACATION = "vacation", "휴가"
-    LEISURE = "leisure", "놀이"
-    PARENTS = "parents", "친정"
+    VACATION = "VACATION", "휴가"
+    LEISURE = "LEISURE", "놀이"
+    PARENTS = "PARENTS", "친정"
     # 운동
-    EXERCISE_GOODS = "exercise_goods", "운동용품"
-    EXERCISE_FEE = "exercise_fee", "운동이용료"
+    EXERCISE_GOODS = "EXERCISE_GOODS", "운동용품"
+    EXERCISE_FEE = "EXERCISE_FEE", "운동이용료"
     # 미상
-    UNKNOWN = "unknown", "미상"
+    UNKNOWN = "UNKNOWN", "미상"
     # 청약
-    SUBSCRIPTION = "subscription", "청약"
+    SUBSCRIPTION = "SUBSCRIPTION", "청약"
     # 자기개발
-    LEARNING = "learning", "학습"
+    LEARNING = "LEARNING", "학습"
     # 경조사
-    CELEBRATION = "celebration", "경조사"
+    CELEBRATION = "CELEBRATION", "경조사"
     # 기타
-    INSURANCE = "insurance", "보험료"
-    ETC = "etc", "기타"
+    INSURANCE = "INSURANCE", "보험료"
+    ETC = "ETC", "기타"
     # 미정산
-    UNSETTLED = "unsettled", "미정산"
-
+    UNSETTLED = "UNSETTLED", "미정산"
 
 CATEGORY_SUBCATEGORY_MAP: dict[str, list[str]] = {
     ExpenseCategoryEnum.BEAUTY: [
@@ -148,6 +147,6 @@ CATEGORY_SUBCATEGORY_MAP: dict[str, list[str]] = {
 
 
 class ExpensePaymentMethodEnum(models.TextChoices):
-    SHINHAN = "shinhan", "신한Big카드"
-    WOORI = "woori", "우리은행통장"
-    CASH = "cash", "현금"
+    SHINHAN = "SHINHAN", "신한Big카드"
+    WOORI = "WOORI", "우리은행통장"
+    CASH = "CASH", "현금"
