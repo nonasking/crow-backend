ab5fe5de8006e7087477ac58f2382c1227699ee8
kangminsung <nonasking@gmail.com>
2026-03-03 17:19:15 +0900

    test(settings): add testing host

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1',]
 
 
 # Application definition
