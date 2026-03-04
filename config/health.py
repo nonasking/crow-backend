59c7b2b46d317d7f483bcdeafeb1e8e277f5e7b0
kangminsung <nonasking@gmail.com>
2026-03-04 15:26:37 +0900

    feat(health-check): edit health check api renderer

--- config/health.py
+++ config/health.py
@@ -1,11 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
 
 
 class HealthCheckAPIView(APIView):
     authentication_classes = []
     permission_classes = []
+    renderer_classes = [JSONRenderer]
 
     def get(self, request):
         try:
