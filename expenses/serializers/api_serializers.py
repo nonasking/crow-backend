3982682a10e1d07830d3f206361643a4603c3e1a
kangminsung <nonasking@gmail.com>
2026-03-15 16:00:12 +0900

    feat(expenses/views/budget_viewset.py): remove unused api

--- expenses/serializers/api_serializers.py
+++ expenses/serializers/api_serializers.py
@@ -24,38 +24,6 @@ class NotionBudgetMigrateSerializer(serializers.Serializer):
         return NotionClient().migrate_budget_to_db(skip_duplicates=skip_duplicates)
 
 
-class BudgetSummarySerializer(serializers.Serializer):
-    year = serializers.IntegerField(required=False)
-    month = serializers.IntegerField(required=False)
-    category = serializers.CharField(required=False)
-
-    def validate(self, attrs):
-        today = date.today()
-
-        attrs["year"] = attrs.get("year", today.year)
-        attrs["month"] = attrs.get("month", today.month)
-
-        return attrs
-
-    def summary(self):
-        year = self.validated_data["year"]
-        month = self.validated_data["month"]
-        categories = self.validated_data.get("category")
-
-        qs = Budget.objects.filter(year=year, month=month)
-
-        if categories:
-            qs = qs.filter(category__in=categories.split(","))
-
-        total_budget = qs.aggregate(total=Sum("amount"))["total"] or 0
-
-        return {
-            "year": year,
-            "month": month,
-            "total_budget": total_budget,
-        }
-
-
 class ExpenseSummarySerializer(serializers.Serializer):
     year = serializers.IntegerField(required=False)
     month = serializers.IntegerField(required=False)
